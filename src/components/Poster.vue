0bb7cb843e4f268b0dd2e897c156dfb61df102a9
yiyang.chen <yiyang.chen@univers.com>
2024-01-12 17:46:47 +0800

    feat. 开发准备

---
+++ src/components/Poster.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+    <header class="header">
+        <h1></h1>
+    </header>
+</template>
+
+<style scoped>
+.header {
+    height: 100vh;
+    width: 100vw;
+    background-image: url('@/assets/2024/logo.svg');
+}
+</style>
---
+++ src/views/2024.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+import Poster from '@/components/Poster.vue'
+</script>
+
+<template>
+    <Poster />
+    
+</template>
+
+<style scoped>
+</style>
