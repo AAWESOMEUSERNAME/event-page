73190cffbf8e04ea191fba0f439a4d571a74dea5
gugugu <17621718276@163.com>
2024-02-24 10:15:14 +0800

    feat. 删除废弃组件，图片大小调整

--- src/constants/instructors.ts
+++ src/constants/instructors.ts
@@ -4,23 +4,16 @@ import imgRamona from '@/assets/images/instructors/Ramona_c.jpg'
 import imgNathan from '@/assets/images/instructors/Nathan_c.jpg'
 import imgZeng from '@/assets/images/instructors/Zeng_c.jpg'
 import imgBei from '@/assets/images/instructors/Bei_c.jpg'
-import imgRyanBW from '@/assets/images/instructors/Ryan.jpg'
-import imgSingBW from '@/assets/images/instructors/Sing.jpg'
 import imgRamonaBW from '@/assets/images/instructors/Ramona.jpg'
-import imgNathanBW from '@/assets/images/instructors/Nathan.jpg'
-import imgZengBW from '@/assets/images/instructors/Zeng.jpg'
-import imgBeiBW from '@/assets/images/instructors/Bei.jpg'
 
 export default {
     Ryan: {
         img: imgRyan,
-        imgBW: imgRyanBW,
         name: 'Ryan Francois',
         intro: 'Ryan Francois is a Jazz Dancer, Producer, Director, Choreographer, Instructor and Historian. A two-time US Open and American Swing Dance Champion and the recipient of the Frankie Manning Award. A pioneer of the revitalisation of Vernacular jazz. He is internationally known for popularising the dance styles of the Charleston and the Lindy hop for the primetime television shows Strictly Come Dancing, Dancing with the Stars and So You Think You Can Dance, and for his choreography and leading role in the Broadway show “SWING!” (1999). His work on “SWING has been nominated for a Tony Award, Drama Desk and Fred Astaire Award and saw him perform at the 2000 Tony Awards at Radio City Music Hall. He is the creative mind behind Swing School & The Authentic Jazz Academy and is the founder and Director of 3 dance companies Swing X-Treme, Swing Un- Ltd and Zoots and Spangles.'
     },
     Sing: {
         img: imgSing,
-        imgBW: imgSingBW,
         name: 'Sing Yuen Lim',
         intro: 'Sing found her lindy spirit when she met Ryan Francois in London in 1988. This soon developed into a serious hobby, competing and winning trophies in the US Open Swing Dance Championships and the American Lindy Hop Championships in 1992 & 1993 and running Jitterbugs London in Leicester Square every week.In 1995 Sing moved back home to Singapore and started a lindy hop scene called Jitterbugs Swingapore. Before long, several instructors including Frankie Manning, Chazz Young, Erin Stevens, Lennart Westerlund, Ewa “W” Burak, Minnies Moochers, visited to teach and perform. Since then, Sing continues to play an active role in the community, on and off the dance floor. In 2014 she was one of the organisers of Frankie 100 in NYC and in Singapore, she heads a weekly team at Timbre Substation with a live swing band and classes. A mother of 3 kids, she still travels Asia and the world to share her interest in the roots and history of Lindy hop, while finding time to compete and to coach performance groups like the Pineapple Tarts and the Pineapple Jam. She teaches in Herrang Dance Camp for many years, not just for Lindy hop, but also the Teachers Track.\n\rShe counts her influences as Frankie Manning, Chazz Young, Norma Miller and the pioneer dancers of the 1930s as well as the many strong amazing dancers like Angela Andrew and LaTasha Barnes.'
     },
@@ -32,19 +25,16 @@ export default {
     },
     Nathan: {
         img: imgNathan,
-        imgBW: imgNathanBW,
         name: 'Nathan Bugh',
         intro: 'Nathan Bugh is one of the world\'s leading exponents of Lindy Hop and jazz dance. His on-stage appearances include: Carnegie Hall (Swing Swing Swing w/ New York Pops), The Apollo Theater (Swingin’ Frankie’s Way), and Jacob’s Pillow (Earthbeat!, More Forever, SW!NG OUT). Active in the Lindy Hop scene since the 1990\'s, he has earned numerous first-place titles in Solo Jazz, Lindy Hop, Team, Invitational, Slow Dance, and Mix/Match divisions at events like ILHC (International Lindy Hop Championships), ULHS, and ALHC. He was also the choreographer of the 2015 ILHC winning team, "Yeah Man.” In 2016, he joined Caleb Teicher for "Meet Ella," a lauded duet incorporating Lindy Hop with other jazz/theater styles. The Joyce Theater then asked them to create "SW!NG OUT," which became the greatest Lindy Hop proscenium show of a generation.'
     },
     Zeng: {
         img: imgZeng,
-        imgBW: imgZengBW,
         name: 'Weiming Zeng',
         intro: '关注摇摆和爵士的根源，不断从老视频中获取灵感，注重摇摆舞本源的探索与表达。热衷分享，在教学中善于与学生们一起探索，共同为之注入新的活力。充满激情，有独特的亲和力，受到各地舞者的欢迎与喜爱。是中国最具有影响力的摇摆舞者和导师之一，期待与你在舞池中相遇。'
     },
     Bei: {
         img: imgBei,
-        imgBW: imgBeiBW,
         name: 'Beibei Huang',
         intro: '她从非洲根源中收获了巨大的鼓舞，是一个十分崇尚自然能量和平衡的舞者。学习和分享摇摆的历程，也不断的打开她对世界的认知，学习历史文化，学习与人交流，提升自我认知，她的成长和摇摆的探究密不可分。极具亲和力和感染力的她同时是一个逻辑型的教师，她善于把获取的灵感由浅入深的传递给学生，以节奏为魔法，身体为智慧，带领大家一起享受摇摆的世界。'
     }
